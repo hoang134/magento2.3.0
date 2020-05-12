de1ec581eca78f3bd518d2e095ee88cf33ba8315
hoang <hoangha13499.kshb@gmail.com>
2020-05-12 21:41:30 +0700

    them_tiengviet

---
+++ app/code/Codilar/HelloWorld/Controller/Index/Hello.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Codilar\HelloWorld\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Hello extends Action
+{
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory
+    ) {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        $page = $this->pageFactory->create();
+        return $page;
+    }
+}
